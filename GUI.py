fef8374c9135d4ee3a237b1adef28175c37bf79c
Norwin Wilden <90617804+norwin64@users.noreply.github.com>
2022-05-22 18:44:24 +0200

    try

---
+++ GUI.py
@@ -0,0 +1,76 @@
+#python vorlage
+import pygame
+import os
+from Button import Button
+
+#init
+pygame.init()
+#root
+root = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+FPS = 60
+WIDTH,HEIGHT = root.get_width(), root.get_height()
+
+#fonts
+font_h = pygame.font.SysFont("maiandragd", 70, bold=True)
+font_2 = pygame.font.SysFont("maiandragd", 40)
+
+
+#colours
+BLACK = (0, 0, 0)
+BLUE = (0, 150, 255)
+
+#variables
+
+#images
+background = pygame.transform.scale(pygame.image.load(os.path.join("materials", "background.png")), (WIDTH, HEIGHT))
+background_2 = pygame.transform.scale(pygame.image.load(os.path.join("materials", "background_2.png")), (WIDTH-100, HEIGHT-100))
+exit_image = pygame.transform.scale(pygame.image.load(os.path.join("materials", "exit.png")), (70, 70))
+
+#widgets
+EXIT_BUTTON = Button(WIDTH-140, 70, exit_image, 1)
+more_stacks = Button(1/10 * WIDTH, 700, font_2.render("Click here for more units", True, BLUE), 0.8)
+
+
+
+def show_window():
+    root.blit(background, (0, 0))
+    root.blit(background_2, (50, 50))
+    if EXIT_BUTTON.draw(root):
+        quit()
+
+    # "Your stacks"
+    root.blit(font_h.render("Your stacks:", True, BLACK), (1/10 * WIDTH, 210))
+    # Stacks
+    with open("elements.txt", "r") as file:
+        stacks = file.read().splitlines()
+    for element in stacks:
+        if stacks.index(element) <= 2:
+            spot = 360 + 110 * stacks.index(element)
+            BUTTON = Button(1 / 10 * WIDTH, spot, font_2.render(f"*  {element}", True, BLACK), 1)
+            if BUTTON.draw(root):
+                root.blit(background_2, (50, 50))
+
+        else:
+            if more_stacks.draw(root):
+                print("weitere stacks")
+
+    pygame.display.update()
+
+
+
+def quit():
+    pygame.display.quit()
+    exit()
+
+def main():
+    run = True
+    while run:
+        pygame.time.Clock().tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        show_window()
+    quit()
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,117 @@
+# Vocab_Trainer
+import csv
+import pandas as pd
+from datetime import date, timedelta
+import random
+
+
+
+def create_list():
+    # What name should the list have
+    name = input("Wie ist der Name der Liste? ")
+
+    # Create a list with the given name
+    df = pd.DataFrame(columns=["german_word", "english_word", "correct_in_a_row", "date_of_next_question"])
+    df.to_csv(path_or_buf=rf"C:\Users\Norwi\Desktop\Python\vocab_trainer\Trainer (2.0)\{name}.csv", index=False)
+    #Switch to Add vocab function
+    add_vocab(name)
+
+def add_vocab(name):
+    # Ask for vocabs
+    german_vocab = input("Wie lautet die deutsche Vokabel? ")
+    if german_vocab == "leave":        # Currently the only way to end the function
+        return
+    english_vocab = input("Wie lautet die englische Vokabel? ")
+    # Add the vocabs to the csv file
+    with open(f"{name}.csv", "a") as file:
+        writer(file).writerow([german_vocab, english_vocab, 0, date.today()])
+    # Repeat the function
+    add_vocab(name)
+
+def edit_list(name):
+    # Choice between "delete vocab", "Change vocab", "Add vocab"
+    choose = input("What would you like to do? (d = delete_vocab, c = change_vocab, a = add_vocab)")
+    df = pd.read_csv(f"{name}.csv")
+    print(df)
+    if choose == "d":       # Delete a vocab
+        while True:
+            ind = input("type in the index of the vocab, which you would like to delete: ")
+            col = input("type in the type of the vocab, which you would like to delete: ")
+            if ind == "leave":
+                break
+            df = df.drop(int(ind))
+    if choose == "c":       # Change a vocab
+        while True:
+            ind = input("type in the index of the vocab, which you would like to change: ")
+            col = input("type in the type of the vocab, which you would like to delete: ")
+            if ind == "leave":
+                break
+            new_input = input("What should the word look like?")
+            df.at[int(ind), col] = new_input
+    if choose == "a":       # Add a vocab
+        add_vocab(name)
+    df.to_csv(path_or_buf=fr"C:\Users\Norwi\Desktop\Python\vocab_trainer\Trainer (2.0)\{name}.csv", index=False)
+
+
+def ask_vocab(vocab):
+    german = vocab.get("german_word")
+    english = vocab.get("english_word")
+    answer = input(f"What is the english word for {german}?")
+    if answer == english:
+        print("yeah")
+        return True
+    else:
+        print("nope")
+        return False
+
+def elimination_mode(name):
+    wrong = []
+    with open(f"{name}.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            wrong.append(row)
+    while len(wrong) > 0:
+        random_number = random.randint(0, len(wrong)-1)
+        random_vocab = wrong[random_number]
+        if ask_vocab(random_vocab):
+            wrong.remove(random_vocab)
+
+def vocab_test(name):
+    list = []
+    vocab = 0
+    right = []
+    with open(f"{name}.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            list.append(row)
+    if len(list) > 10:
+         while vocab != 10:
+             random_number = random.randint(0, len(list) - 1)
+             random_vocab = list[random_number]
+             if ask_vocab(random_vocab):
+                 right.append(random_number)
+             list.remove(random_vocab)
+             vocab += 1
+    else:
+        print("Sry, but the chosen Unit is not big enough to create a vocab test")
+
+def karteikarten_system(name):
+    df = pd.read_csv(f"{name}.csv")
+    list = []
+    for index in df.index:
+        if df["date_of_next_question"][index] == str(date.today()):
+            list.append(index)
+    while len(list) != 0:
+        random_number = list[random.randint(0, len(list) - 1)]
+        random_vocab = df.loc[random_number]
+        if ask_vocab(random_vocab):
+            df.loc[random_number, "correct_in_a_row"]  = int(df.loc[random_number, "correct_in_a_row"]) + 1
+            next_day = int(df.loc[random_number, "correct_in_a_row"]) * 2
+            df.loc[random_number, "date_of_next_question"] = date.today() + timedelta(next_day)
+        else:
+            df.loc[random_number, "correct_in_a_row"] = 0
+            df.loc[random_number, "date_of_next_question"] = date.today() + timedelta(1)
+    list.remove(random_number)
+    df.to_csv(path_or_buf=fr"C:\Users\Norwi\Desktop\Python\vocab_trainer\Trainer (2.0)\{name}.csv", index=False)
+
+
